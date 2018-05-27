fef573793f7539b194ec9e2b66b3da682abb0049
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-27 21:28:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,15 @@
+#def oxford_comma(array)
+#if array.size == 1
+#array.join
+
+#end
+
 def oxford_comma(array)
-array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "and" => "pomelos"]
-array.join
-puts results
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
